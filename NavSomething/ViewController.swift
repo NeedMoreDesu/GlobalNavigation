9bf815e5b22be7e760d3bf5117f3c1f0e39820ef
Oleksii Horishnii <oleksii.horishnii@gmail.com>
2017-05-25 21:10:26 +0300

    Initial Commit

---
+++ NavSomething/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavSomething
+//
+//  Created by Oleksii Horishnii on 5/25/17.
+//  Copyright © 2017 Oleksii Horishnii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
